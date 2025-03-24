f3591fed2f55afcecd60e8be54e1767458e6d25d
2020b-Krisztin Benedek <144203768+b0kriben@users.noreply.github.com>
2025-03-24 15:56:09 +0100

    Add files via upload

---
+++ ScholarshipApplicationProject.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using ScholarshipApplicationProject.Console.Models;
+using ScholarshipApplicationProject.Console.Repo;
+
+
+Console.WriteLine("1. feladat");
+try
+{
+    ScholarshipApplicant empty = new ScholarshipApplicant("", "Null");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("2. feladat");
+ScholarshipApplicant odon = new ScholarshipApplicant("odon@nyertes.hu", "Ösztöndíj Ödön");
+Console.WriteLine(odon);
+
+Console.WriteLine("3. feladat");
+try
+{
+    odon.Win(-30000);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("4. feladat");
+odon.Win(20000);
+odon.Win(30000);
+Console.WriteLine(odon);
+
+ScholarshipRepo repo = new();
+
+Console.WriteLine($"Ösztöndíjasok száma: {repo.GetNumberOfScholarship()}");
---
+++ ScholarshipApplicationProject.Console/Repo/ScholarshipRepo.cs
@@ -0,0 +1,20 @@
+﻿using ScholarshipApplicationProject.Console.DbModels;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScholarshipApplicationProject.Console.Repo
+{
+    public class ScholarshipRepo
+    {
+        private readonly ScholarshipContext _context = new();
+
+        public int GetNumberOfScholarship()
+        {
+            return _context.Scholarships.Count();
+        }
+    }
+}
